5bce89afdc0c809eef1e7e2df9c2fc2823481eef
domssab <saballo.chantaldominique@gmail.com>
2022-11-12 22:19:11 +0800

    main menu options added

---
+++ contact-tracing.py
@@ -0,0 +1,13 @@
+# Printing menu
+print("Contact Tracing"
+      "\nMenu: "
+      "\n1 -> Add an item"
+      "\n2 -> Search"
+      "\n3 -> Exit")
+
+# Input for menu
+mainmenu = int(input("Choose what to do: "))
+
+#Main menu
+mainmenu = 0
+while mainmenu in (0,1,2,3)
